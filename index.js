dffd1a28ab6b6aa436a2d4651344d68637b4f5fc
Sherzod <sariyevsherzod77@gmail.com>
2023-06-14 16:33:38 +0500

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+function greet(firstName, lastName){
+    console.log('Assalom ' + firstName + ' ' + lastName);
+}
+function square(number){
+    return number * number;
+}
+
+
+greet('Ali', 'Valiyev');
+greet('Vali', 'Valiyev');
+greet('bunyod', 'Soriyev');
+greet('jamshid');
+console.log(square(9));
